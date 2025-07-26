2dfc7c2cac13c16b01922fdfe6dca34473b0fbb1
sushichan044 <71284054+sushichan044@users.noreply.github.com>
2025-07-27 01:00:00 +0900

    add util to suppress log from callback fn

--- packages/eslint-config-array-resolver/src/utils.ts
+++ packages/eslint-config-array-resolver/src/utils.ts
@@ -32,3 +32,64 @@ export const isNonEmptyString = (
 };
 
 type Awaitable<T> = Promise<T> | T;
+
+/**
+ *  Execute a function with silent logs.
+ *
+ * `console.warn`, `console.error`, and `console.trace` will not be silenced.
+ * @param function_
+ * @returns
+ */
+export const executeWithSilentLogs = async <T>(
+  function_: () => Awaitable<T>,
+): Promise<T> => {
+  const original = {
+    debug: console.debug,
+    info: console.info,
+    log: console.log,
+  };
+
+  console.log = NO_OP_FN;
+  console.info = NO_OP_FN;
+  console.debug = NO_OP_FN;
+
+  let res:
+    | {
+        data: T;
+        err: null;
+      }
+    | {
+        data: typeof NO_RESULT;
+        err: Error;
+      } = {
+    data: NO_RESULT,
+    err: new Error("No result returned from the callback"),
+  };
+
+  try {
+    res = {
+      data: await function_(),
+      err: null,
+    };
+  } catch (error) {
+    if (error instanceof Error) {
+      res.err = error;
+    } else {
+      res.err = new Error(String(error));
+    }
+  } finally {
+    console.log = original.log;
+    console.info = original.info;
+    console.debug = original.debug;
+  }
+
+  if (res.err) {
+    throw res.err;
+  }
+
+  return res.data;
+};
+
+// eslint-disable-next-line @typescript-eslint/no-empty-function
+const NO_OP_FN = () => {};
+const NO_RESULT = Symbol("NO_RESULT");
