46ca7409f2d346675384fcfe1332292bb7b36bff
Kentaro Suzuki <71284054+sushichan044@users.noreply.github.com>
2026-02-03 20:58:25 +0900

    fix: output temp files to cwd to avoid bundling module under `node_modules` (#40)
    * prevent eager bundling poc
    
    * poc: bundle unrun
    
    * no notice

--- packages/eslint-config-array-resolver/tsdown.config.ts
+++ packages/eslint-config-array-resolver/tsdown.config.ts
@@ -1,17 +1,38 @@
+import license from "rollup-plugin-license";
 import { defineConfig } from "tsdown";
-
 export default defineConfig({
   attw: { level: "error", profile: "esm-only" },
   clean: true,
   dts: true,
   entry: ["src/index.ts"],
+  external: ["rolldown"],
   fixedExtension: true,
   format: "esm",
+  inlineOnly: ["unrun"],
   minify: "dce-only",
   nodeProtocol: true,
+  noExternal: [
+    // We need to bundle unrun to apply our patch fix.
+    // But we don't want to bundle rolldown, so we mark it as external and mark as dependecy in package.json.
+    "unrun",
+  ],
   outDir: "dist",
+  plugins: [
+    license({
+      thirdParty: {
+        output: {
+          file: "dist/THIRD-PARTY.txt",
+        },
+      },
+    }),
+  ],
   publint: true,
   sourcemap: false,
   treeshake: true,
-  unused: true,
+  unused: {
+    enabled: true,
+    ignore: {
+      dependencies: ["rolldown"],
+    },
+  },
 });
